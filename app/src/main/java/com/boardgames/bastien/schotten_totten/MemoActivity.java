cbe0e7948f3687f3f106bfd357a56d534ecfccfe
Bastien Barthet <bastien.barthet@gmail.com>
2017-03-30 20:00:02 +0200

    mise a jour du boutton close du memo

--- app/src/main/java/com/boardgames/bastien/schotten_totten/MemoActivity.java
+++ app/src/main/java/com/boardgames/bastien/schotten_totten/MemoActivity.java
@@ -43,7 +43,7 @@ public class MemoActivity extends AppCompatActivity {
         findViewById(R.id.closeButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+               onBackPressed();
             }
         });
 
