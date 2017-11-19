bc9833c56ba95fd8ced14d0609ef8159d495bf26
Bastien Barthet <bastien.barthet@gmail.com>
2017-11-19 10:39:50 +0100

    add Springboot lan game manager

--- app/src/main/java/com/boardgames/bastien/schotten_totten/controllers/LanGameManager.java
+++ app/src/main/java/com/boardgames/bastien/schotten_totten/controllers/LanGameManager.java
@@ -1,17 +1,105 @@
 package com.boardgames.bastien.schotten_totten.controllers;
 
+import com.boardgames.bastien.schotten_totten.exceptions.EmptyDeckException;
 import com.boardgames.bastien.schotten_totten.exceptions.GameCreationException;
+import com.boardgames.bastien.schotten_totten.exceptions.HandFullException;
+import com.boardgames.bastien.schotten_totten.exceptions.MilestoneSideMaxReachedException;
+import com.boardgames.bastien.schotten_totten.exceptions.NoPlayerException;
+import com.boardgames.bastien.schotten_totten.exceptions.NotYourTurnException;
+import com.boardgames.bastien.schotten_totten.model.Card;
+import com.boardgames.bastien.schotten_totten.model.Milestone;
+import com.boardgames.bastien.schotten_totten.model.Player;
+import com.boardgames.bastien.schotten_totten.model.PlayingPlayerType;
 
-import java.util.concurrent.Callable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Bastien on 19/10/2017.
  */
 
-public class LanGameManager extends SimpleGameManager {
+@SpringBootApplication
+public class LanGameManager {
 
-    public LanGameManager(final String player1Name, final String player2Name) throws GameCreationException {
-        super(player1Name, player2Name, System.currentTimeMillis() + "");
+    private SimpleGameManager gameManager;
+    private ConfigurableApplicationContext context;
+
+    public void start() {
+        context = SpringApplication.run(LanGameManager.class);
+    }
+
+    public void stop() {
+        SpringApplication.exit(context);
+    }
+
+    @RestController
+    private class Controlers {
+
+        @RequestMapping("/ping")
+        public String ping() {
+            return new Date().toString() + " - it is time to SCHOTTEN !!!!";
+        }
+
+        @RequestMapping("/createGame")
+        public boolean createGame() {
+            try {
+                gameManager = new SimpleGameManager("Player 1", "Player 2");
+                return true;
+            } catch (final GameCreationException e) {
+                return false;
+            }
+        }
+
+        @RequestMapping("/getLastPlayedCard")
+        public Card getLastPlayedCard() {
+            return gameManager.getLastPlayedCard();
+        }
+
+        @RequestMapping("/reclaimMilestone")
+        public boolean reclaimMilestone(
+                @RequestParam(value="p") PlayingPlayerType p,
+                @RequestParam(value="milestoneIndex") int milestoneIndex) throws NotYourTurnException {
+            return gameManager.reclaimMilestone(p, milestoneIndex);
+        }
+
+        @RequestMapping("/getPlayer")
+        public Player getPlayer(
+                @RequestParam(value="p") PlayingPlayerType p) {
+            return gameManager.getPlayer(p);
+        }
+
+        @RequestMapping("/getPlayingPlayer")
+        public Player getPlayingPlayer() {
+            return gameManager.getPlayingPlayer();
+        }
+
+        @RequestMapping("/getWinner")
+        public Player getWinner() throws NoPlayerException {
+            return gameManager.getWinner();
+        }
+
+        @RequestMapping("/getMilestones")
+        public List<Milestone> getMilestones() {
+            return gameManager.getMilestones();
+        }
+
+        @RequestMapping("/playerPlays")
+        public void playerPlays(
+                @RequestParam(value="p") PlayingPlayerType p,
+                @RequestParam(value="indexInPlayingPlayerHand") int indexInPlayingPlayerHand,
+                @RequestParam(value="milestoneIndex") int milestoneIndex)
+                throws NotYourTurnException, EmptyDeckException, HandFullException, MilestoneSideMaxReachedException {
+
+            gameManager.playerPlays(p, indexInPlayingPlayerHand, milestoneIndex);
+        }
+
     }
 
 }
---
+++ app/src/test/java/com/boardgames/bastien/schotten_totten/LanGameManagerTest.java
@@ -0,0 +1,36 @@
+package com.boardgames.bastien.schotten_totten;
+
+import com.boardgames.bastien.schotten_totten.controllers.LanGameManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Bastien on 19/11/2017.
+ */
+
+public class LanGameManagerTest {
+
+    private static LanGameManager manager;
+
+    @BeforeClass
+    public static void Before() {
+        manager = new LanGameManager();
+        manager.start();
+    }
+
+    @AfterClass
+    public static void After() {
+        manager.stop();
+    }
+
+    @Test
+    public void TestPing() {
+        final RestTemplate rest = new RestTemplate();
+        System.out.println(rest.getForObject("http://localhost:8080/ping", String.class).toString());
+    }
+}
